958950f41b7471248c5aa9af1befc206d9d1d800
Antti Jaakkola <antti.jaakkola@finnhems.fi>
2015-05-11 11:35:18 +0300

    fixed dns-plugin to work also with servers given by dns-name

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,20 @@
 # encoding: utf-8
+import socket
+
 
 def sanitize(n):
     return n.replace('.', '_').replace(':', '_')
+
+
+def get_ipv6_by_name(name):
+    try:
+        return socket.getaddrinfo(name, 0, socket.AF_INET6)[0][4][0]
+    except socket.gaierror:
+        return None
+
+
+def get_ipv4_by_name(name):
+    try:
+        return socket.gethostbyname(name)
+    except socket.gaierror:
+        return None
