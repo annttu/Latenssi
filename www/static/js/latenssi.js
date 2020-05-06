4632531e99154c55bea87db142f25df76192b98e
Antti Jaakkola <antti.jaakkola@remod.fi>
2020-05-06 17:17:45 +0300

    Fix interval guessing with variable hour

--- www/static/js/latenssi.js
+++ www/static/js/latenssi.js
@@ -1,4 +1,3 @@
-
 function update_images()
 {
     var images = [].slice.call(document.getElementsByTagName("img"));
@@ -14,7 +13,7 @@ function update_images()
 }
 
 var current_time = {};
-var interval = 3600;
+var step = 3600;
 
 function goback(img) {
    imgid = "img-" + img;
@@ -22,24 +21,25 @@ function goback(img) {
        var seconds = Math.floor(new Date().getTime() / 1000);
        current_time[imgid] = seconds;
    }
-   current_time[imgid] -= interval;
+   current_time[imgid] -= step;
    goImage(imgid, current_time[imgid]);
 }
 
-function guessInterval(interval) {
-    if (interval.includes("hour")) {
-       hours = parseInt(interval.substr(0, interval.length-4))
+function guessInterval(stepname) {
+    if (stepname == "hour") return 3600;
+    if (stepname.includes("hour")) {
+       hours = parseInt(stepname.substr(0, stepname.length-4))
        return hours * 3600;
     }
-    if (interval == "day") return 24*3600;
-    if (interval == "week") return 7*24*3600;
-    if (interval == "month") return 30*24*3600;
-    if (interval == "year") return 365*24*3600;
+    if (stepname == "day") return 24*3600;
+    if (stepname == "week") return 7*24*3600;
+    if (stepname == "month") return 30*24*3600;
+    if (stepname == "year") return 365*24*3600;
     return 3600;
 }
 
 function goImage(imgid, end) {
-   var start = end - interval;
+   var start = end - step;
    imgElement = document.getElementById(imgid);
    var currentURL = new URL(imgElement.src);
    currentURL.search = "?start=" + start + "&end=" + end;
@@ -47,13 +47,15 @@ function goImage(imgid, end) {
 }
 function goforward(img) {
    imgid = "img-" + img;
-   current_time[imgid] += interval;
+   if (!(imgid in current_time)) {
+
+   }
+   current_time[imgid] += step;
    goImage(imgid, current_time[imgid]);
 }
 
 function latenssi() {
     setInterval(update_images, 300000);
     x = document.location.pathname.split("/");
-    interval = guessInterval(x[x.length-1]);
+    step = guessInterval(x[x.length-1]);
 }
-
